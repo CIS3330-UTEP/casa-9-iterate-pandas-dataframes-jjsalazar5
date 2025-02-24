9dc17bf4449a722ce9cf9088ad36acf7360277ee
jjsalazar5 <jjsalazar3@miners.utep.edu>
2025-02-23 23:57:05 -0700

    casa 9

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,15 @@
+import pandas as pd
+df = pd.read_csv("big-mac-full-index.csv")
+
+#method 4
+print("\nInteracting using interrows():")
+for index, row in df.iterrows():  
+    print(f"Index: {index}: {row.to_dict()}")
+
+
+#method 6
+print("\nIterating using apply():")
+def process_row(row):
+    print(f"Processed row: {row.to_dict()}")
+df.apply(process_row, axis=1)
 
